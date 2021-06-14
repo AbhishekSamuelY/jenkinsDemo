9dcad1b74dc9e0c6a83ec66028ee1e8c7b1144d4
abhishek <62393400+AbhishekSamuelY@users.noreply.github.com>
2021-06-15 02:03:21 +0530

    "first commit"

---
+++ DemoDockerJenkinsInt/src/main/java/com/abhishek/demoDockerJenkinsInt/DemoDockerJenkinsIntApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.demoDockerJenkinsInt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerJenkinsIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerJenkinsIntApplication.class, args);
+	}
+
+}
